61e9fb9d855979cd76b1f194d7471ee6aaea69af
Viki-Ren <renvicki77@gmail.com>
2025-02-16 17:51:38 +0800

    Update DV0101EN-Final-Assign-Part-2-Questions.py

--- DV0101EN-Final-Assign-Part-2-Questions.py
+++ DV0101EN-Final-Assign-Part-2-Questions.py
@@ -165,7 +165,7 @@ def update_output_container(selected_statistics,input_year):
             title='Average Vehicles Sold by Vehicle Type in the year {}'.format(input_year)))
 
             # Total Advertisement Expenditure for each vehicle using pie chart
-        exp_data=yearly_data.groupby('Vehicle_Type')['Vehicle_Type','Advertising_Expenditure'].sum().reset_index()
+        exp_data=yearly_data.groupby('Vehicle_Type')['Advertising_Expenditure'].sum().reset_index()
         Y_chart4 = dcc.Graph(figure=px.pie(exp_data,
             values = 'Advertising_Expenditure',
             names = 'Vehicle_Type',
@@ -184,3 +184,4 @@ def update_output_container(selected_statistics,input_year):
 # Run the Dash app
 if __name__ == '__main__':
     app.run_server(debug=True)
+
